9c9886e52039115f9cd9cb0c29e1d578ab2c2f42
Tuomas Kontola <tuomas.kontola@columbiaroad.com>
2021-07-31 15:47:42 +0300

    Modifies instructions, modifies date

--- src/pages/Instruction.tsx
+++ src/pages/Instruction.tsx
@@ -8,12 +8,13 @@ const Instructions = () => (
     <div>
       <ol>
         <li>Ilmoita haluamasi lippumäärä luokittain. Yhteensä yli 30 lipun tilaaminen ei ole mahdollista yhteen osoitteeseen.</li>
-        <li>Liput maksetaan verkkomaksulla. Mikäli tämä ei onnistu voit vaihtaa maksutavaksi sähköpostilaskun.
-          Jos sinulla on erityistoiveita paperi laskulle, ilmoita siitä tilauksen lisätiedoissa.</li>
-        <li>Valitse, haluatko liput lisämaksusta postitse kotiin vai e-lippuna antamaasi sähköpostiosoitteeseen.</li>
         <li>Täytä seuraavaksi yhteystietosi. Täytä jokainen kohta.</li>
         <li>Täytä mahdolliset erityistoiveesi lippujen suhteen.</li>
         <li>Siirry maksamaan.</li>
+        <li>Valitse, haluatko liput lisämaksusta postitse kotiin vai e-lippuna antamaasi sähköpostiosoitteeseen.</li>
+        <li>Liput maksetaan verkkomaksulla. Mikäli tämä ei onnistu voit vaihtaa maksutavaksi sähköpostilaskun.
+          Jos sinulla on erityistoiveita paperi laskulle, ilmoita siitä tilauksen lisätiedoissa.</li>
+        <li>Lahjakortteja voi lisätä niin monta tilaukseen kuin haluaa. Varmista, että lahjakorttiin liittyvä lippu löytyy ostoskorista</li>
         <li>Valitse maksutapa</li>
         <li>Maksa lippuostokset</li>
         <li>Odota tilaus vahvistusta. Toimitamme liput, kun olemme saaneet kaikki tilaukset käsiteltyä.</li>
