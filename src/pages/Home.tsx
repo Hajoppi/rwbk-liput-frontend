aa796ebdeafa5e38b7a7599f5b0d7cfeaf66b0a1
Tuomas Kontola <tuomas.kontola@columbiaroad.com>
2021-09-05 18:36:14 +0300

    More info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,9 @@ const Home = () => {
       Tervetuloa tilaamaan lippuja Retuperän WBK:n konserttiin! Konsertti järjestetään Finlandia-talossa 5.11.2021 klo 19:33.
       Tilaus tapahtuu allaolevalla lomakkeella, minkä jälkeen käsittelemme kaikki tilaukset ja lähetämme liput valitsemallanne toimitustavalla.
       </p>
+      <p>Valitkaa haluamanne lippujen määrä kustakin lippuluokasta.
+        Mikäli ostatte eri lippuluokan lippuja, huomatkaa, että eri lippuluokan liput
+        plaseerataan katsomossa eri paikkoille lukuunottamatta opiskelija ja II-luokan lippuja, jotka voidaan plaseerta yhteen.</p>
       <p>
         Jos olet epävarma kuinka toimia, ole hyvä ja <StyledLink to="/ohjeet">katso tarkemmat ohjeet.</StyledLink>
       </p>
